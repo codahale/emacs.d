79719810c9490440d5d8b53acec7e20445ad16d7
Coda Hale <coda.hale@gmail.com>
2013-10-28 17:29:41 -0700

    Include MELPA, I guess.

--- init.el
+++ init.el
@@ -3,6 +3,9 @@
 (add-to-list 'package-archives 
     '("marmalade" .
       "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+    '("melpa" .
+      "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
 (setq default-tab-width 4)
