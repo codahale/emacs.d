587bef8744441d4b78c7a3e647da1430018e784a
Coda Hale <coda.hale@gmail.com>
2014-03-28 17:46:42 -0700

    Cargo cult some quietness on startup.

--- init.el
+++ init.el
@@ -1,3 +1,13 @@
+;;;; INITIAL
+
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; No splash screen please ... jeez
+(setq inhibit-startup-message t)
+
 ;;;; CASK
 
 (require 'cask "/usr/local/Cellar/cask/0.6.0/cask.el")
