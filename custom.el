399fbe588131ecf9b5988fe85b59beee171026f8
Coda Hale <coda.hale@gmail.com>
2014-06-03 16:10:30 -0700

    Ignore Godeps directory in Projectile.

--- custom.el
+++ custom.el
@@ -6,11 +6,13 @@
  '(custom-safe-themes (quote ("4a60f0178f5cfd5eafe73e0fc2699a03da90ddb79ac6dbc73042a591ae216f03" default)))
  '(flycheck-disabled-checkers (quote (emacs-lisp-checkdoc ruby-rubylint)))
  '(legalese-default-author "Coda Hale")
- '(legalese-default-copyright "coda.hale@gmail.com"))
+ '(legalese-default-copyright "coda.hale@gmail.com")
+ '(projectile-globally-ignored-directories (quote (".idea" ".eunit" ".git" ".hg" ".fslckout" ".bzr" "_darcs" ".tox" ".svn" "build" "Godeps"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:foreground "#DCDCCC" :background "#3F3F3F"))))
  '(flycheck-fringe-error ((t (:background "dark red"))))
  '(region ((t (:background "black" :foreground "white" :inverse-video t)))))
