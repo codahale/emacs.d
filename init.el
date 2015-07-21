5ffbed7907b0a4a365d9a0c1a3cf3ada7d7f7889
Coda Hale <coda@stripe.com>
2015-07-21 10:02:38 -0700

    Add Helm config.

--- init.el
+++ init.el
@@ -115,6 +115,7 @@
                            " HI2"
                            " Abbrev"
                            " Interactive"
+                           " Helm"
                            " WK"
                            )))
 
@@ -266,6 +267,32 @@
 
 ;; use paredit
 (add-hook 'lisp-mode-hook 'paredit-mode)
+
+;;;; HELM
+
+(require 'helm)
+(require 'helm-config)
+
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+
+(helm-mode 1)
 
 ;;;; COCOA
 
@@ -455,13 +482,18 @@
 
 (global-set-key (kbd "C-c c")       'compile)
 (global-set-key (kbd "C-c g")       'magit-status)
-(global-set-key (kbd "C-c i")       'helm-imenu)
 (global-set-key (kbd "C-c l p")     'list-packages)
 (global-set-key (kbd "C-c r")       'recompile)
 (global-set-key (kbd "C-c t")       'coda/visit-term-buffer)
 
+;; helm bindings
 (global-set-key (kbd "C-c M-x")     'execute-extended-command) ; old M-x
 (global-set-key (kbd "M-x")         'helm-M-x)
+(global-set-key (kbd "M-y")         'helm-show-kill-ring)
+(global-set-key (kbd "C-c i")       'helm-imenu)
+(global-set-key (kbd "C-x b")       'helm-mini)
+(global-set-key (kbd "C-x C-f")     'helm-find-files)
+(global-set-key (kbd "C-c h o")     'helm-occur)
 
 ;; unmap upcase-region, since it always screws with undo
 (global-unset-key (kbd "C-x C-u"))
