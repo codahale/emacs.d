a6b143de902201c26a64dbbfdeb937e2fc1869bc
Coda Hale <coda.hale@gmail.com>
2015-07-21 19:17:01 -0700

    Use regular Helm tab behavior.

--- init.el
+++ init.el
@@ -207,10 +207,6 @@
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
 
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
-(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
-(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-
 (when (executable-find "curl")
   (setq helm-google-suggest-use-curl-p t))
 
