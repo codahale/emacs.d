8e21198407ddccf1d34296651ccf6183a522af1a
Coda Hale <coda.hale@gmail.com>
2015-07-21 19:40:46 -0700

    Add Helm keybindings.

--- init.el
+++ init.el
@@ -424,16 +424,29 @@
 (global-set-key (kbd "C-c t")       'coda/visit-term-buffer)
 
 ;; helm bindings
-(global-set-key (kbd "C-x C-b")     'helm-buffers-list)
-(global-set-key (kbd "C-x C-r")     'helm-recentf)
-(global-set-key (kbd "C-x r l")     'helm-filtered-bookmarks)
 (global-set-key (kbd "C-c M-x")     'execute-extended-command) ; old M-x
+(global-set-key (kbd "C-x C-d")     'helm-browse-project)
+(global-set-key (kbd "C-h C-f")     'helm-apropos)
+(global-set-key (kbd "C-h r")       'helm-info-emacs)
+(global-set-key (kbd "C-h i")       'helm-info-at-point)
+(global-set-key (kbd "C-:")         'helm-eval-expression-with-eldoc)
+(global-set-key (kbd "C-,")         'helm-calcul-expression)
+(global-set-key (kbd "C-x C-b")     'helm-buffers-list)
+(global-set-key (kbd "C-c f")       'helm-recentf)
+(global-set-key (kbd "C-x C-f")     'helm-find-files)
 (global-set-key (kbd "M-x")         'helm-M-x)
 (global-set-key (kbd "M-y")         'helm-show-kill-ring)
 (global-set-key (kbd "C-c i")       'helm-imenu)
 (global-set-key (kbd "C-x b")       'helm-mini)
 (global-set-key (kbd "C-x C-f")     'helm-find-files)
 (global-set-key (kbd "C-c h o")     'helm-occur)
+
+(define-key global-map [remap jump-to-register]      'helm-register)
+(define-key global-map [remap list-buffers]          'helm-buffers-list)
+(define-key global-map [remap dabbrev-expand]        'helm-dabbrev)
+(define-key global-map [remap find-tag]              'helm-etags-select)
+(define-key global-map [remap xref-find-definitions] 'helm-etags-select)
+
 
 ;; unmap upcase-region, since it always screws with undo
 (global-unset-key (kbd "C-x C-u"))
