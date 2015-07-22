f31e2625c6c23d3e09f25e74a6a47f3218eb0259
Coda Hale <coda.hale@gmail.com>
2015-07-21 19:40:26 -0700

    Add key bindings for killing buffers and files.

--- init.el
+++ init.el
@@ -220,6 +220,9 @@
       helm-yank-symbol-first                 t
       )
 
+(define-key helm-find-files-map (kbd "C-d") 'helm-ff-persistent-delete)
+(define-key helm-buffer-map (kbd "C-d")     'helm-buffer-run-kill-persistent)
+
 (helm-descbinds-install)
 (helm-projectile-on)
 (helm-adaptive-mode t)
