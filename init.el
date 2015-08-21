00bd516374ab8e38cb12f28b635e7859a04cdecb
Coda Hale <coda.hale@gmail.com>
2015-08-20 18:44:24 -0700

    Remove magit fullscreen advice.
    Split screen's actually pretty nice these days.

--- init.el
+++ init.el
@@ -309,23 +309,6 @@
 (eval-after-load 'flycheck
   '(add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
-;;;; MAGIT
-
-(require 'magit)
-
-(defadvice magit-status (around magit-fullscreen activate)
-  (window-configuration-to-register :magit-fullscreen)
-  ad-do-it
-  (delete-other-windows))
-
-(defun magit-quit-session ()
-  "Restores the previous window configuration and kills the magit buffer"
-  (interactive)
-  (kill-buffer)
-  (jump-to-register :magit-fullscreen))
-
-(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
-
 ;;;; JAVASCRIPT
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
