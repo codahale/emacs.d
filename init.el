91f722c7304811215364d89402ec70e350ce2c39
Coda Hale <coda.hale@gmail.com>
2015-10-17 09:18:04 -0700

    Add helm-flx.

--- init.el
+++ init.el
@@ -139,6 +139,8 @@
 
 ;;;; HELM
 
+(helm-flx-mode +1)
+
 (require 'helm)
 (require 'helm-config)
 (require 'helm-files)
@@ -169,36 +171,6 @@
 (helm-projectile-on)                    ; integrate w/ projectile
 (helm-adaptive-mode t)                  ; use adaptive mode to rank common items
 (helm-mode 1)                           ; enable helm!
-
-(with-eval-after-load 'helm
-  ;; make sure you have flx installed
-  (require 'flx)
-  ;; this is a bit hackish, ATM, redefining functions I don't own
-  (defvar helm-flx-cache (flx-make-string-cache #'flx-get-heatmap-str))
-
-  (defun helm-score-candidate-for-pattern (candidate pattern)
-    (or (car (flx-score candidate pattern helm-flx-cache)) 0))
-
-  (defun helm-fuzzy-default-highlight-match (candidate)
-    (let* ((pair (and (consp candidate) candidate))
-           (display (if pair (car pair) candidate))
-           (real (cdr pair)))
-      (with-temp-buffer
-        (insert display)
-        (goto-char (point-min))
-        (if (string-match-p " " helm-pattern)
-            (cl-loop with pattern = (split-string helm-pattern)
-                     for p in pattern
-                     do (when (search-forward p nil t)
-                          (add-text-properties
-                           (match-beginning 0) (match-end 0) '(face helm-match))))
-          (cl-loop with pattern = (cdr (flx-score display
-                                                  helm-pattern helm-flx-cache))
-                   for index in pattern
-                   do (add-text-properties
-                       (1+ index) (+ 2 index) '(face helm-match))))
-        (setq display (buffer-string)))
-      (if real (cons display real) display))))
 
 ;;;; COCOA
 
